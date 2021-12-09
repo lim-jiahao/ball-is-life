bb55892bc6e938fcea58f0542113b29f4de5f99d
lim-jiahao <lim.jiahaoo@gmail.com>
2021-12-09 16:00:21 +0800

    put routes in separate routes.js file

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ import methodOverride from 'method-override';
 import dotenv from 'dotenv';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import homeRouter from './routers/homeRouter.js';
-import loginRouter from './routers/loginRouter.js';
-import signupRouter from './routers/signupRouter.js';
-import logoutRouter from './routers/logoutRouter.js';
-import predictionRouter from './routers/predictionRouter.js';
-import leaderboardRouter from './routers/leaderboardRouter.js';
-import gameRouter from './routers/gameRouter.js';
-import userRouter from './routers/userRouter.js';
-import dayRouter from './routers/dayRouter.js';
-import chatRouter from './routers/chatRouter.js';
-import aboutRouter from './routers/aboutRouter.js';
+import bindRoutes from './routes.js';
 import scheduler from './utils/schedule.js';
 
 dotenv.config();
@@ -29,17 +19,7 @@ app.use(cookieParser());
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-app.use('/', homeRouter);
-app.use('/login', loginRouter);
-app.use('/signup', signupRouter);
-app.use('/logout', logoutRouter);
-app.use('/prediction', predictionRouter);
-app.use('/leaderboard', leaderboardRouter);
-app.use('/game', gameRouter);
-app.use('/user', userRouter);
-app.use('/day', dayRouter);
-app.use('/chat', chatRouter);
-app.use('/about', aboutRouter);
+bindRoutes(app);
 
 io.on('connection', (socket) => {
   let chatRoom = 'default';
